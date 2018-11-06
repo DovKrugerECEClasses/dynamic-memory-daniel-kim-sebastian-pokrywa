9eecdd205e8441d95f53ddbe3f18aa47d3bafd2b
Daniel Kim <DanKim8@hotmail.com>
2018-11-06 14:09:36 -0500

    Added header file for color class

---
+++ dynamicmemory/bitmap/color.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class Color {
+private:
+	uint32_t rgba;
+public:
+
+	//Black, Green, Blue, Red, White
+	Color(uint8_t r, uint8_t g, uint8_t b) {
+		rgba = (r << 16) + (g << 8) + b;
+		}
+	
+	char returnColor() const{
+		if (rgba == 0xFF0000){
+			return 'r';
+		}
+		else if (rgba == 0x00FF00) {
+			return 'g';
+		}
+		else if (rgba == 0x0000FF) {
+			return 'b';
+		}
+		else{
+			return 'B';
+		}
+	}
+	
+};
