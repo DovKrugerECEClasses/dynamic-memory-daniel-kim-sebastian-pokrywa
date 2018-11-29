998f15e05adbbacf89b2c9035b6b461f5621ddcb
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2018-11-29 18:15:08 -0500

    Finished load function of hashmap

--- standardlibrary/danstdlib.cc
+++ standardlibrary/danstdlib.cc
@@ -4,6 +4,7 @@
 #include <map>
 #include <unordered_map>
 #include <iterator>
+#include <string>
 
 using namespace std;
 
@@ -38,26 +39,31 @@ void printMap (map<string, double> m){
 	}
 }
 
+void load (map<string, double> &m, string name){
+	string key;
+	double value;
+
+	ifstream f(name);
+	while (f>>key>>value){
+		m.insert(pair <string, double> (key, value));
+	}
+}
+
 int main() {
 //	vector<int> list = buildVector(5, 10, 1000); // from 5 stepping by 10 to 1000
 //	removeMultiples(list, 3); // remove all numbers that are multiples of 3
 //	print(list);
-	double n;
-	string stock;
 
-	ifstream f("2f.dat");
-	f >> n;
+//	map<string,double> m;
 
-	map<string,double> m;
-
-	load(m, "stocks.dat"); // load the stock quotes into the map m
+//	load(m, "stocks.dat"); // load the stock quotes into the map m
 	// write code to print out the map.  Because it is a tree,
 	// it will print in sorted order
-	printMap(m);
+//	printMap(m);
 
 //	cout << m["AAPL"] << '\n'; // O(log n)
 	 
-//	unordered_map<string,double> hash;
+	unordered_map<string,double> hash;
 //	load(hash, "stocks.dat"); // load the stock quotes into the unordered_map hash
 	// write code to print out the hashmap.  It will be in a random order because
 	// hashmaps are not sorted (but they are faster for lookup).
